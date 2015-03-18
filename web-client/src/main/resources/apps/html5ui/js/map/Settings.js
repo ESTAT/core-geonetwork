699d251037692b19db7a53ae72309fdd28f386d3
Peter Riisager <priisager@gmail.com>
2015-03-18 17:15:38 +0100

    changing the background map
    https://webgate.ec.europa.eu/CITnet/jira/browse/GISCOMETAP-53

--- web-client/src/main/resources/apps/html5ui/js/map/Settings.js
+++ web-client/src/main/resources/apps/html5ui/js/map/Settings.js
@@ -28,33 +28,58 @@ OpenLayers.Util.onImageLoadError = function() {
 // +units=m +no_defs";
 Proj4js.defs["EPSG:2154"] = "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
 
+Proj4js.defs["EPSG:3857"] = Proj4js.defs["EPSG:3857"] = "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+
 GeoNetwork.map.printCapabilities = "../../pdf";
 
 // Config for WGS84 based maps
-GeoNetwork.map.PROJECTION = "EPSG:4326";
-GeoNetwork.map.EXTENT = new OpenLayers.Bounds(-180,-90,180,90);
+GeoNetwork.map.PROJECTION = "EPSG:3857";
+GeoNetwork.map.EXTENT = new OpenLayers.Bounds(-550000, 5000000, 1200000, 7000000);
 //GeoNetwork.map.EXTENT = new OpenLayers.Bounds(-5.1,41,9.7,51);
 
 
 
-//GeoNetwork.map.BACKGROUND_LAYERS = [
-//    //new OpenLayers.Layer.WMS("Background layer", "/geoserver/wms", {layers: 'gn:world,gn:ne_50m_boundary_da,gn:ne_50m_boundary_lines_land,gn:ne_50m_coastline', format: 'image/jpeg'}, {isBaseLayer: true})
-//    new OpenLayers.Layer.WMS("Background layer", "http://www2.demis.nl/mapserver/wms.asp?", {layers: 'Countries', format: 'image/jpeg'}, {isBaseLayer: true})
-//];
-
+GeoNetwork.map.BACKGROUND_LAYERS = [
+    //new OpenLayers.Layer.WMS("Background layer", "/geoserver/wms", {layers: 'gn:world,gn:ne_50m_boundary_da,gn:ne_50m_boundary_lines_land,gn:ne_50m_coastline', format: 'image/jpeg'}, {isBaseLayer: true}),
+   // new OpenLayers.Layer.WMS("Background layer", "http://www2.demis.nl/mapserver/wms.asp?", {layers: 'Countries', format: 'image/jpeg'}, {isBaseLayer: true})
+    new OpenLayers.Layer.WMS(
+            "Global Imagery",
+            "https://webgate.acceptance.ec.europa.eu/estat/inspireec/gis/arcgis/services/Basemaps/2014_Countries_NoLabels_GreyBackground_3857/MapServer/WMSServer?",
+            {layers: "0,1,2,3,4,5,6,7,8,9,10,11,12,13"}
+        ),
+];
+//https://webgate.acceptance.ec.europa.eu/estat/inspireec/gis/arcgis/rest/services/Basemaps/Blue_marble_3857/MapServer/WMSServer?
+//	http://data.geus.dk/gis/arcgis/services/GtW/S014_Jordartskort_200000/MapServer/WMSServer?
 // Config for OSM based maps
 if (useOSMLayers) {
-    GeoNetwork.map.PROJECTION = "EPSG:900913";
+    GeoNetwork.map.PROJECTION = "EPSG:3857";
     //GeoNetwork.map.EXTENT = new OpenLayers.Bounds(-550000, 5000000, 1200000, 7000000);
     GeoNetwork.map.EXTENT = new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34);
     GeoNetwork.map.BACKGROUND_LAYERS = [
-        new OpenLayers.Layer.OSM()
-        //new OpenLayers.Layer.Google("Google Streets");
+        
+ //       new OpenLayers.Layer.Google("Google Streets"),
+        new OpenLayers.Layer.WMS(
+                "Global Imagery",
+                "https://webgate.acceptance.ec.europa.eu/estat/inspireec/gis/arcgis/services/Basemaps/2014_Countries_NoLabels_GreyBackground_3857/MapServer/WMSServer?",
+                {layers: "0,1,2,3,4,5,6,7,8,9,10,11,12,13"}
+            ),
+       new OpenLayers.Layer.OSM()
     ];
+    
+
 } else {
-    GeoNetwork.map.BACKGROUND_LAYERS = [];
-    GeoNetwork.map.CONTEXT = "../../maps/mapviewer.wmc";
-    //GeoNetwork.map.OWS = "../../maps/demis.xml";
+	 GeoNetwork.map.PROJECTION = "EPSG:3857";
+	    //GeoNetwork.map.EXTENT = new OpenLayers.Bounds(-550000, 5000000, 1200000, 7000000);
+	    GeoNetwork.map.EXTENT = new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34);
+	    GeoNetwork.map.BACKGROUND_LAYERS = [
+	        
+	 //       new OpenLayers.Layer.Google("Google Streets"),
+	        new OpenLayers.Layer.WMS(
+	                "Global Imagery",
+	                "https://webgate.acceptance.ec.europa.eu/estat/inspireec/gis/arcgis/services/Basemaps/2014_Countries_NoLabels_GreyBackground_3857/MapServer/WMSServer?",
+	                {layers: "0,1,2,3,4,5,6,7,8,9,10,11,12,13"}
+	            )
+	    ];
 }
 
 //GeoNetwork.map.RESOLUTIONS = [];
