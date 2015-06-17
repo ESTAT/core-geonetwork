898457ad6fb78180f67387d3a36a3108dbcacc7e
María Arias de Reyna <maria.arias@geocat.net>
2015-06-17 17:02:05 +0200

    Updating version too

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v21031/migrate-db-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v21031/migrate-db-default.sql
@@ -1,2 +1,4 @@
 insert into Settings (select max(id) + 1, 1, 'statistics', NULL from Settings);
 insert into Settings (select id+1, id, 'snippet', '' from Settings where name like 'statistics');
+UPDATE Settings SET value='2.10.3.1' WHERE name='version';
+UPDATE Settings SET value='SNAPSHOT' WHERE name='subVersion';
