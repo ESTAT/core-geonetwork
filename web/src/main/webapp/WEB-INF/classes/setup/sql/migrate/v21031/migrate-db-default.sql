dfcc7af3bd3003c6d02d72f87f7c42df999d8f0a
María Arias de Reyna <maria.arias@geocat.net>
2015-06-29 09:48:54 +0200

    GISCOMETAP-105 Harvesting error message: migration scripts improved to fix errors

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v21031/migrate-db-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v21031/migrate-db-default.sql
@@ -1,4 +1,9 @@
-insert into Settings (select max(id) + 1, 1, 'statistics', NULL from Settings);
-insert into Settings (select id+1, id, 'snippet', '' from Settings where name like 'statistics');
+DELETE FROM Settings WHERE parentid IN (
+        SELECT id FROM (SELECT * FROM Settings) 
+            AS a WHERE name LIKE 'statistics'
+    );   
+DELETE FROM Settings WHERE name LIKE 'statistics';
+INSERT INTO Settings (SELECT max(id) + 1, 1, 'statistics', NULL FROM Settings);
+INSERT INTO Settings (SELECT id+1, id, 'snippet', '' FROM Settings WHERE name LIKE 'statistics');
 UPDATE Settings SET value='2.10.3.1' WHERE name='version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='subVersion';
