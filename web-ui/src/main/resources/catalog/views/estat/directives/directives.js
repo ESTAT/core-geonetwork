b49ed641c16b4e7dc771dd4c7bfaa07fba6a2d73
jesperschmidt <jesper.schmidt@atkinsglobal.com>
2016-02-09 19:08:04 +0100

    Contact page added, top link, guest and anonymous message on home page

--- web-ui/src/main/resources/catalog/views/estat/directives/directives.js
+++ web-ui/src/main/resources/catalog/views/estat/directives/directives.js
@@ -129,4 +129,20 @@
       };
     }
   ]);
+  
+  module.directive('scrollToItem', [
+     function() {                                                      
+	    return {                                                                                 
+	        restrict: 'A',                                                                       
+	        scope: {                                                                             
+	            scrollTo: "@"                                                                    
+	        },                                                                                   
+	        link: function(scope, $elm,attr) {                                                   
+	            $elm.on('click', function() {                              
+	                $('html,body').animate({scrollTop: $(scope.scrollTo).offset().top }, "slow");
+	            });                                                                              
+	        }                                                                                    
+	    }
+	  }
+     ])       
 })();
