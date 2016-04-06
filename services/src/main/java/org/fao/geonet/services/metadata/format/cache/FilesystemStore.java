9ddf4366bcbe7db4067756de892c2e01bbc0602f
Jose Garcia <jose.garcia@geocat.net>
2016-04-06 11:34:59 +0200

    Use AUTO_SERVER in formatter cache H2 database, to check if solves issues with lock in ESTAT environment

--- services/src/main/java/org/fao/geonet/services/metadata/format/cache/FilesystemStore.java
+++ services/src/main/java/org/fao/geonet/services/metadata/format/cache/FilesystemStore.java
@@ -85,7 +85,7 @@ public class FilesystemStore implements PersistentStore {
                     "CREATE TABLE IF NOT EXISTS " + STATS_TABLE + " (" + NAME + " VARCHAR(64) PRIMARY KEY, " + VALUE + " VARCHAR(32) NOT NULL)"
 
             };
-            String init = ";INIT=" + Joiner.on("\\;").join(initSql) + ";DB_CLOSE_DELAY=-1";
+            String init = ";INIT=" + Joiner.on("\\;").join(initSql) + ";DB_CLOSE_DELAY=-1;AUTO_SERVER=TRUE";
             String dbPath = testing ? "mem:" + UUID.randomUUID() : getBaseCacheDir().resolve("info-store").toString();
             metadataDb = DriverManager.getConnection("jdbc:h2:" + dbPath + init, "fsStore", "");
 
